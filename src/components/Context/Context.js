3246b175adbf7911a30ed807a13284eb802ce208
ahmedali0159 <pirates0159@gmail.com>
2021-06-07 13:44:32 +0600

    add redirects file

---
+++ src/components/Context/Context.js
@@ -0,0 +1,17 @@
+// import { createContext, useState } from "react";
+
+// const Context = createContext()
+// export const useMyContext = () => {
+//     return useContext(Context);
+// }
+
+
+// export const ContextProvider = ({children}) => {
+
+// }
+
+// const ContextProvider = () => { 
+//     return <div></div>
+// };
+
+// export default ContextProvider;
--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import React from "react";
 
 const Profile = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <div>
+      <div>
+        
+      </div>
+      <div class="container mt-5">
+      <div class="mb-3">
+        <h3 className="text-center">Add New Post</h3>
+        <label for="formGroupExampleInput" class="form-label">
+          Add Post
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Example input placeholder"
+        />
+      </div>
+      <div class="mb-3">
+        <label for="formGroupExampleInput2" class="form-label">
+          Another label
+        </label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Another input placeholder"
+        />
+      </div>
+    </div>
+    </div>
+  );
 };
 
 export default Profile;
